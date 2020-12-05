da4b5638fc0e34b7eed1d3c4353abf9f9bfbb561
ElijahCoding <cuiyeqing960904@gmail.com>
2020-12-05 23:55:43 +0300

    endpoints built

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -9,6 +9,6 @@ class LogoutController extends Controller
 {
     public function __invoke()
     {
-
+        auth()->logout();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', 'LoginController');
-Route::post('/logout', 'LogoutController');
+Route::post('/login', LoginController::class);
+Route::post('/logout', LogoutController::class);
