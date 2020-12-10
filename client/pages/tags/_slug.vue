cbaa10c94b57d41f9f73eb98f7837d6a548da7be
ElijahCoding <cuiyeqing960904@gmail.com>
2020-12-10 23:01:41 +0300

    tag by slug

--- client/pages/tags/_slug.vue
+++ client/pages/tags/_slug.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
-
+    <div class="mt-10">
+      <job v-for="job in jobs" :key="job.id" :job="job"/>
+    </div>
   </div>
 </template>
 
 <script>
 import ALL_JOBS_BY_TAG from '@/graphql/AllJobsByTag.gql'
+import TAG_BY_SLUG from '@/graphql/TagBySlug.gql'
 
 export default {
   apollo: {
+    tag: {
+      query: TAG_BY_SLUG
+    },
+
     jobs: {
+      variables() {
+        return {
+          slug: this.$route.params.slug
+        }
+      },
       query: ALL_JOBS_BY_TAG,
     }
   }
