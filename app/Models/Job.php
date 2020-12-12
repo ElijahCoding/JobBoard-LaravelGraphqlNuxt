734c7864b79e48e58aa830e767ff59ed60b43edd
ElijahCoding <cuiyeqing960904@gmail.com>
2020-12-13 00:00:20 +0300

    register user

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,17 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
 
     public function tags()
     {
         return $this->belongsToMany(Tag::class);
     }
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
